f5ffaf4864a47d72c4c5d39778a9fe69c0829c19
Sunny Rain <765677193@qq.com>
2026-03-03 12:39:35 +0800

    Delete miri_test_results directory

--- miri_test_results/fixed_alloc__stack_free.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = 42;
-    // Correctly allocate on the heap and copy the value of x
-    let good_box = Box::new(x);
-    // good_box will be safely deallocated here or via explicit drop
-    drop(good_box);
-}
